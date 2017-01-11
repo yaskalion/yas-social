1b9fdbd053a2eabb2b70de5baca7600f255ffa19
yaskalion <yaskalion@hotmail.com>
2017-01-11 22:35:44 +0300

    Sign in ui custom classes complete

---
+++ yas-social/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  yas-social
+//
+//  Created by Yasin Abdullahi on 11/01/17.
+//  Copyright © 2017 Yasin Abdullahi. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY : CGFloat = 120.0 / 255.0
+
---
+++ yas-social/RoundedBtn.swift
@@ -0,0 +1,37 @@
+//
+//  RoundedBtn.swift
+//  yas-social
+//
+//  Created by Yasin Abdullahi on 11/01/17.
+//  Copyright © 2017 Yasin Abdullahi. All rights reserved.
+//
+
+import UIKit
+
+class RoundedBtn: UIButton {
+    
+    override func awakeFromNib() {
+    super.awakeFromNib()
+        
+       
+        layer.shadowColor = UIColor (red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width:1.0, height:1.0)
+        imageView?.contentMode = .scaleToFill
+        
+        
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = self.frame.width / 2.7
+        
+
+    
+        
+    }
+
+}
